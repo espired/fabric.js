4fded91c78d3237245acf96ededeae014bb9ac0c
ShaMan123 <shacharnen@gmail.com>
2022-07-31 18:27:29 +0300

    Update observable.mixin.ts

--- src/mixins/observable.mixin.ts
+++ src/mixins/observable.mixin.ts
@@ -1,145 +1,141 @@
-(function(global) {
-  var fabric = global.fabric;
-  /**
-   * @private
-   * @param {String} eventName
-   * @param {Function} handler
-   */
-  function _removeEventListener(eventName, handler) {
-    if (!this.__eventListeners[eventName]) {
-      return;
-    }
-    var eventListener = this.__eventListeners[eventName];
-    if (handler) {
-      eventListener[eventListener.indexOf(handler)] = false;
-    }
-    else {
-      fabric.util.array.fill(eventListener, false);
-    }
-  }
+//@ts-nocheck
+type EventRegistryObject = { [eventName: string]: Function };
+
+
+/**
+ * @tutorial {@link http://fabricjs.com/fabric-intro-part-2#events}
+ * @see {@link http://fabricjs.com/events|Events demo}
+ */
+export class Observable {
+
+  private __eventListeners: { [eventName: string]: Function[] } = {};
 
   /**
    * Observes specified event
-   * @memberOf fabric.Observable
    * @alias on
    * @param {String|Object} eventName Event name (eg. 'after:render') or object with key/value pairs (eg. {'after:render': handler, 'selection:cleared': handler})
    * @param {Function} handler Function that receives a notification when an event of the specified type occurs
    * @return {Function} disposer
    */
-  function on(eventName, handler) {
+  on(eventName: string, handler: Function): Function
+  on(handlers: EventRegistryObject): Function
+  on(arg0: string | EventRegistryObject, handler?: Function): Function {
     if (!this.__eventListeners) {
-      this.__eventListeners = { };
+      this.__eventListeners = {};
     }
     // one object with key/value pairs was passed
-    if (arguments.length === 1) {
-      for (var prop in eventName) {
-        this.on(prop, eventName[prop]);
+    if (typeof arg0 === 'object') {
+      for (var prop in arg0) {
+        this.on(prop, arg0[prop]);
       }
     }
     else {
-      if (!this.__eventListeners[eventName]) {
-        this.__eventListeners[eventName] = [];
+      if (!this.__eventListeners[arg0]) {
+        this.__eventListeners[arg0] = [];
       }
-      this.__eventListeners[eventName].push(handler);
+      this.__eventListeners[arg0].push(handler!);
     }
-    return off.bind(this, eventName, handler);
-  }
-
-  function _once(eventName, handler) {
-    var _handler = function () {
-      handler.apply(this, arguments);
-      this.off(eventName, _handler);
-    }.bind(this);
-    this.on(eventName, _handler);
-    return _handler;
+    return () => this.off(arg0, handler);
   }
 
   /**
    * Observes specified event **once**
-   * @memberOf fabric.Observable
    * @alias once
-   * @param {String|Object} eventName Event name (eg. 'after:render') or object with key/value pairs (eg. {'after:render': handler, 'selection:cleared': handler})
+   * @param {String|Object} arg0 Event name (eg. 'after:render') or object with key/value pairs (eg. {'after:render': handler, 'selection:cleared': handler})
    * @param {Function} handler Function that receives a notification when an event of the specified type occurs
    * @return {Function} disposer
    */
-  function once(eventName, handler) {
+  once(eventName: string, handler: Function): Function
+  once(handlers: EventRegistryObject): Function
+  once(arg0: string | EventRegistryObject, handler?: Function): Function {
     // one object with key/value pairs was passed
-    if (arguments.length === 1) {
-      var handlers = {};
-      for (var prop in eventName) {
-        handlers[prop] = _once.call(this, prop, eventName[prop]);
+    if (typeof arg0 === 'object') {
+      const disposers: Function[] = [];
+      for (let prop in arg0) {
+        const _handler = arg0[prop];
+        const disposer = this.on(prop, (...args: any[]) => {
+          _handler.apply(this, args);
+          this.off(prop, disposer);
+        });
+        disposers.push(disposer);
       }
-      return off.bind(this, handlers);
+      return () => disposers.forEach(d => d());
     }
     else {
-      var _handler = _once.call(this, eventName, handler);
-      return off.bind(this, eventName, _handler);
+      const disposer = this.on(arg0, (...args: any[]) => {
+        handler!.apply(this, args);
+        this.off(arg0, disposer);
+      });
+      return disposer;
+    }
+  }
+
+  /**
+   * @private
+   * @param {string} eventName 
+   * @param {Function} [handler] 
+   */
+  private _removeEventListener(eventName: string, handler?: Function) {
+    if (!this.__eventListeners[eventName]) {
+      return;
+    }
+    if (handler) {
+      const eventListener = this.__eventListeners[eventName];
+      const index = eventListener.indexOf(handler);
+      eventListener.splice(index, 1);
+    }
+    else {
+      this.__eventListeners[eventName] = [];
     }
   }
 
   /**
    * Stops event observing for a particular event handler. Calling this method
    * without arguments removes all handlers for all events
-   * @memberOf fabric.Observable
-   * @alias off
    * @param {String|Object} eventName Event name (eg. 'after:render') or object with key/value pairs (eg. {'after:render': handler, 'selection:cleared': handler})
    * @param {Function} handler Function to be deleted from EventListeners
    */
-  function off(eventName, handler) {
+  off(eventName: string, handler: Function): void
+  off(handlers: EventRegistryObject): void
+  off(arg0?: string | EventRegistryObject, handler?: Function) {
     if (!this.__eventListeners) {
       return;
     }
 
     // remove all key/value pairs (event name -> event handler)
-    if (arguments.length === 0) {
-      for (eventName in this.__eventListeners) {
-        _removeEventListener.call(this, eventName);
+    if (typeof arg0 === 'undefined') {
+      for (arg0 in this.__eventListeners) {
+        this._removeEventListener(arg0);
       }
     }
     // one object with key/value pairs was passed
-    else if (typeof eventName === 'object' && typeof handler === 'undefined') {
-      for (var prop in eventName) {
-        _removeEventListener.call(this, prop, eventName[prop]);
+    else if (typeof arg0 === 'object') {
+      for (var prop in arg0) {
+        this._removeEventListener(prop, arg0[prop]);
       }
     }
     else {
-      _removeEventListener.call(this, eventName, handler);
+      this._removeEventListener(arg0, handler);
     }
+
   }
 
   /**
    * Fires event with an optional options object
-   * @memberOf fabric.Observable
    * @param {String} eventName Event name to fire
    * @param {Object} [options] Options object
    */
-  function fire(eventName, options) {
+  fire(eventName: string, options: object) {
     if (!this.__eventListeners) {
       return;
     }
 
-    var listenersForEvent = this.__eventListeners[eventName];
+    var listenersForEvent = this.__eventListeners[eventName]?.concat();
     if (!listenersForEvent) {
       return;
     }
-
     for (var i = 0, len = listenersForEvent.length; i < len; i++) {
-      listenersForEvent[i] && listenersForEvent[i].call(this, options || { });
+      listenersForEvent[i].call(this, options || {});
     }
-    this.__eventListeners[eventName] = listenersForEvent.filter(function(value) {
-      return value !== false;
-    });
   }
-
-  /**
-   * @namespace fabric.Observable
-   * @tutorial {@link http://fabricjs.com/fabric-intro-part-2#events}
-   * @see {@link http://fabricjs.com/events|Events demo}
-   */
-  fabric.Observable = {
-    fire: fire,
-    on: on,
-    once: once,
-    off: off,
-  };
-})(typeof exports !== 'undefined' ? exports : window);
+}
